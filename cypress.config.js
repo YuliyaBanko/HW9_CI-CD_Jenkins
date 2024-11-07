92f7a14c84f6e40dbfb8c85613cbc39ae4c676b3
Yuliya Banko <yuliyabanko@gmail.com>
2024-11-07 10:39:49 +0300

    chromeWebSecurity: false

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     projectId: "3hrf6z",
+    chromeWebSecurity: false,
     env: {
         username: "yuliya",  
         password: "Zuzu",
