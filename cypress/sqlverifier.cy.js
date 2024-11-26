616039967c275ae774416c77889d1403fa12e06a
Yuliya Banko <yuliyabanko@gmail.com>
2024-11-26 19:13:17 +0300

    delete useless tests

--- cypress/sqlverifier.cy.js
+++
@@ -1,8 +0,0 @@
-describe('VerifierPageLoad', () => {
-    it('should load the page', () => {
-      cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc'); 
-      cy.get('.brand-title')
-        .should('be.visible'); 
-    });
-  });
-  
